d528ca49b695474fe0359de2ac6b7bc5a6b20374
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2021-11-30 15:21:40 +0300

    first script and objects created

---
+++ 2D Project Test/Assets/Scripts/GameControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    public void ButtonClicked()
+    {
+        Debug.Log("Yes Clicked");
+    }
+    
+    void Update()
+    {
+        
+    }
+}
