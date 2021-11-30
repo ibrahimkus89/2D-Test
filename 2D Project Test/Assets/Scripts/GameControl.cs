7a54e3b9a494d6d603c21da8c4df320da7cc6d3a
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2021-11-30 17:51:17 +0300

    level 1 objects and functions created

--- 2D Project Test/Assets/Scripts/GameControl.cs
+++ 2D Project Test/Assets/Scripts/GameControl.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
-    
+    int first_choice_value;
+    GameObject selected_button;
+    GameObject button_itself;
+    public Sprite default_sprite;
+
+
     void Start()
     {
-        
+        first_choice_value = 0;
     }
 
-    public void ButtonClicked()
+    public void Give_object(GameObject myobject)
     {
-        Debug.Log("Yes Clicked");
+        button_itself = myobject;
+        button_itself.GetComponent<Image>().sprite = button_itself.GetComponentInChildren<SpriteRenderer>().sprite;
+    }
+
+    public void ButtonClicked(int value)
+    {
+        //Debug.Log("Yes Clicked : incoming value : " +value);
+
+        if (first_choice_value==0)
+        {
+            first_choice_value= value;
+        }
+        else
+        {
+            if (first_choice_value==value)
+            {
+                Debug.Log("Yes matched");
+                first_choice_value = 0;
+            }
+            else
+            {
+                Debug.Log("didn't match");
+                first_choice_value = 0;
+            }
+        }
     }
     
     void Update()
