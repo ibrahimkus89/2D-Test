70c107824a70a9f562affecf3d2a5a4e30d812e8
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2021-12-02 12:08:59 +0300

    sounds added

--- 2D Project Test/Assets/Scripts/GameControl.cs
+++ 2D Project Test/Assets/Scripts/GameControl.cs
@@ -9,6 +9,8 @@ public class GameControl : MonoBehaviour
     GameObject selected_button;
     GameObject button_itself;
     public Sprite default_sprite;
+    public AudioSource[] sounds;
+    public GameObject[] Buttons;
 
 
     void Start()
@@ -20,38 +22,67 @@ public class GameControl : MonoBehaviour
     {
         button_itself = myobject;
         button_itself.GetComponent<Image>().sprite = button_itself.GetComponentInChildren<SpriteRenderer>().sprite;
+        button_itself.GetComponent<Image>().raycastTarget = false;
+        sounds[1].Play();
+    }
+
+     void Button_situation(bool situation)
+    {
+        foreach (var item in Buttons)
+        {
+            if (item!=null)
+            {
+                item.GetComponent<Image>().raycastTarget = situation;
+            }
+
+            
+        }
     }
 
     public void ButtonClicked(int value)
     {
-        Control(value,button_itself);
+        Control(value);
 
        
     }
     
-     void Control(int incoming_value,GameObject incoming_object)
+     void Control(int incoming_value)
     {
         if (first_choice_value == 0)
         {
             first_choice_value = incoming_value;
-            selected_button = incoming_object;
+            selected_button = button_itself;
         }
         else
         {
-            if (first_choice_value == incoming_value)
-            {
-                Destroy(selected_button.gameObject);
-                Destroy(incoming_object.gameObject);
-                first_choice_value = 0;
-                selected_button = null;
-            }
-            else
-            {
-                selected_button.GetComponent<Image>().sprite = default_sprite;
-                incoming_object.GetComponent<Image>().sprite = default_sprite;
-                first_choice_value = 0;
-                selected_button = null;
-            }
+            StartCoroutine(Check_it(incoming_value));
+        }
+    }
+
+     
+    IEnumerator Check_it(int incoming_value)
+    {
+        Button_situation(false);
+        yield return new WaitForSeconds(1);
+
+        if (first_choice_value == incoming_value)
+        {
+            Destroy(selected_button.gameObject);
+            Destroy(button_itself.gameObject);
+            first_choice_value = 0;
+            selected_button = null;
+            Button_situation(true);
+
+        }
+        else
+        {
+            sounds[2].Play();
+            selected_button.GetComponent<Image>().sprite = default_sprite;
+            button_itself.GetComponent<Image>().sprite = default_sprite;
+            first_choice_value = 0;
+            selected_button = null;
+            Button_situation(true);
+
         }
     }
 }
