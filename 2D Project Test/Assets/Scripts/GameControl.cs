2ed24d7085d4502da797c524ded842c0d1f936d3
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2021-12-02 17:14:14 +0300

    load scene and main menu were created

--- 2D Project Test/Assets/Scripts/GameControl.cs
+++ 2D Project Test/Assets/Scripts/GameControl.cs
@@ -2,22 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
+    public int target_success;
     int first_choice_value;
+    int instant_success;
+    //-------------------------
     GameObject selected_button;
     GameObject button_itself;
+    //-----------------------
     public Sprite default_sprite;
     public AudioSource[] sounds;
     public GameObject[] Buttons;
+    public TextMeshProUGUI counter;
+    public GameObject[] gameoverpanels;
+
+    //Timer
+    float totalTime =3;
+    float minute;
+    float second;
+    bool timer;
+
 
 
     void Start()
     {
         first_choice_value = 0;
+        timer = true;
+        
     }
 
+    
+     void Update()
+    {
+        if (timer && totalTime>1)
+        {
+            totalTime -= Time.deltaTime;
+
+            minute = Mathf.FloorToInt(totalTime / 60);
+            second = Mathf.FloorToInt(totalTime % 60);
+
+            //counter.text = Mathf.FloorToInt(totalTime).ToString();
+            counter.text = string.Format("{0:00}:{1:00}", minute, second);
+        }
+        else
+        {
+            timer = false;
+            GameOver();
+        }
+
+    }
+
+    void GameOver()
+    {
+        gameoverpanels[0].SetActive(true);
+    }
+
+    void Win()
+    {
+        gameoverpanels[1].SetActive(true);
+
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
     public void Give_object(GameObject myobject)
     {
         button_itself = myobject;
@@ -67,12 +125,19 @@ public class GameControl : MonoBehaviour
 
         if (first_choice_value == incoming_value)
         {
-            Destroy(selected_button.gameObject);
-            Destroy(button_itself.gameObject);
+            instant_success++;
+            selected_button.GetComponent<Image>().enabled = false;
+            button_itself.GetComponent<Image>().enabled = false;
+            //selected_button.GetComponent<Button>().enabled = false;
+            //button_itself.GetComponent<Button>().enabled = false;  
             first_choice_value = 0;
             selected_button = null;
             Button_situation(true);
 
+            if (target_success==instant_success)
+            {
+                Win();
+            }
         }
         else
         {
